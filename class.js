a57738ff3448fb9b7aefe19376132fc7c586e99b
rukeme <mydreamcomealive2012@gmail.com>
2021-06-25 11:46:24 +0100

    commit work

---
+++ class.js
@@ -0,0 +1,77 @@
+var x = 5;
+var y = 6;
+var y = 10;
+var z = x + y;
+
+
+console.log(z);
+
+
+var age = 26;
+var rightwrong = true;
+var numChildren = null;
+var definition = undefined;
+
+console.log(definition);
+
+
+
+
+
+var name2 = "oghenerukeme";
+var age = 50;
+var rightwrong = false;
+var numberChildren = null;
+var definition = undefined
+
+//console.log
+console.log(name2);
+console.log(age);
+console.log(rightwrong);
+console.log(numberChildren);
+console.log(definition);
+
+//.innerHTML
+
+// document.getElementById('name').innerHTML = name2;
+document.getElementById('number').innerHTML = age;
+document.getElementById('boolen').innerHTML = rightwrong;
+document.getElementById('null').innerHTML = numberChildren;
+document.getElementById('undefined').innerHTML = definition;
+
+
+
+
+//addition operators
+var number1 = 10;
+var number2 = 20;
+console.log(sum)
+
+
+var sum = number1 + number2;
+
+//multiplication operators
+var a = 5;
+var b = 6;
+var multiple = a * b;
+console.log(multiple)
+
+//subtraction operators
+var h = 20;
+var i = 10;
+var subt = h - i;
+console.log(subt)
+ 
+//division operators
+var m = 65;
+var n = 5;
+var divisoion = m/n;
+console.log(division);
+
+//increment operators
+var s = 10;
+s++;
+console.log('increment operator', s);
+
+
+
